47cec3095ff5e16b04886b30437fdef61212235f
Micah Wendorf <wendorfmicah0@gmail.com>
2021-05-25 18:30:25 -0500

    adding more components and socket io stuff

--- client/src/views/Header.jsx
+++ client/src/views/Header.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = (props) => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div>
-                <h1 className="navbar-brand">NBA Player Rating</h1>
-            </div>
-            <div>
-                <h1 className="navbar-brand"> Online: </h1>
-            </div>
-            <div>
-                <a className="navbar-brand" href="#">Home</a>
-                <select name="" id="">
-                    <option value=""> <a href="">Top 10</a> </option>
-                    <option value=""> <a href="">Top 25</a> </option>
-                </select>
-            </div>
 
-        </nav>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">NBA Player Rating</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Top 50</a>
+                    </li>
+                    
+                </ul>
+                <form className="form-inline my-2 my-md-0">
+                    <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+                </form>
+                
+            </div>
+            </div>
+            </nav>
+        </div>
 );
 };
 
--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const cors = require('cors');
+const server = app.listen(port, () => console.log(`Listening on port: ${port}`));
+const io = require("socket.io")(server, {cors: true})
+// emitters - passes data where it needs to go (emit)
+// on - trigger -- listening for a particular event
+
+// Name of the trigger
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on('chat', msg => {
+        console.log("Got the message: " + msg);
+        io.emit('post chat', msg)
+    })
+})
 
 require('./server/config/mongoose.config');
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
-require('./server/routes/pet.routes')(app);
-
-
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+require('./server/routes/player.routes')(app);
